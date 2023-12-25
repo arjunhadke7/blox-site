30541a85bc1f1eb64a2a1df5cfd04bec09e415c9
Arjun Hadke <arjun.swcm@gmail.com>
2023-12-26 05:03:20 +0530

    changes check on prod site

--- blox-site.php
+++ blox-site.php
@@ -17,6 +17,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+// changes for github reflect push
+
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
