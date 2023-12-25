ee59f2be19522d5526092d4bace70f6e7580ba48
Arjun Hadke <arjun.swcm@gmail.com>
2023-12-26 04:58:36 +0530

    cta button changes

--- build/cta/cta-button/index.asset.php
+++ build/cta/cta-button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2a48aac15292e30e47c5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd0f629aaa4abaa399184');
